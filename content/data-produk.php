0ce1e280659331f59008b5e88ca5711ed07de490
rizkyrazak <creatorrizky@gmail.com>
2021-07-07 15:59:03 +0700

    test

--- content/data-produk.php
+++ content/data-produk.php
@@ -163,7 +163,7 @@ if(isset($_POST['edit_data'])){
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header" id="loginModalLabel">
-                            <h4 class="modal-title">Edit Kategori <?php echo $id_kategori; ?></h4>
+                            <h4 class="modal-title">Edit Produk <?php echo $id_kategori; ?></h4>
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
                           </div>
                           <div class="modal-body">
