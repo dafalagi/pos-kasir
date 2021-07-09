fd26245ae8e2ba2e35b2898931a12c98f2d34131
dafalagi <dafarizky34@gmail.com>
2021-07-09 13:34:07 +0700

    update 9.7.21

--- content/data-produk.php
+++ content/data-produk.php
@@ -132,25 +132,33 @@ if(isset($_POST['edit_data'])){
             </thead>
             <tbody>
             <?php
-                $query = "SELECT * FROM kategori_produk ORDER BY id_kategori DESC";
+                $query = "SELECT * FROM produk ORDER BY id_produk DESC";
                 $dd = $db->prepare($query);
                 $dd->execute();
                 $no = 1;
                 while($row = $dd->fetch(PDO::FETCH_ASSOC)){
+                 $id_produk = $row['id_produk'];
+                 $nama_produk = $row['nama_produk'];
+                 $desc_produk = $row['desc_produk'];
+                 $stok = $row['stok'];
+                 $harga = $row['harga'];
                  $id_kategori = $row['id_kategori'];
-                 $nama_kategori = $row['nama_kategori'];
             ?>
                 <tr>
+                    <td><?php echo $id_produk ?></td>
+                    <td><?php echo $nama_produk ?></td>
+                    <td><?php echo $desc_produk ?></td>
+                    <td><?php echo $stok ?></td>
+                    <td><?php echo $harga ?></td>
                     <td><?php echo $id_kategori ?></td>
-                    <td><?php echo $nama_kategori ?></td>
                     <td>
                     <div class="row">
-                    <button type="button" class="btn btn-dark mb-2 mr-2 rounded-circle" data-toggle="modal" data-target="#edit-kategori-<?php echo $id_kategori; ?>">
+                    <button type="button" class="btn btn-dark mb-2 mr-2 rounded-circle" data-toggle="modal" data-target="#edit-produk-<?php echo $id_produk; ?>">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg> 
                     </button>
                     
                     <form method=POST>
-                      <input type="hidden" name="id_produk" value="<?php echo $id_kategori; ?>"></input>
+                      <input type="hidden" name="id_produk" value="<?php echo $id_produk; ?>"></input>
                       <button type="submit" name="hapus_data" class="btn btn-dark mb-2 mr-2 rounded-circle" onclick="return confirm('Are you sure you want to do that?');">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle table-cancel"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
                       </button>
@@ -158,31 +166,31 @@ if(isset($_POST['edit_data'])){
                     </div>
                     </td>
                 </tr>
-                <!--- MODAL EDIT KATEGORI -->                                
-                <div class="modal fade" id="edit-kategori-<?php echo $id_kategori; ?>" tabindex="-1" role="dialog" aria-labelledby="edit-kategori<?php echo $id_kategori; ?>" aria-hidden="true">
+                <!--- MODAL EDIT -->                                
+                <div class="modal fade" id="edit-produk-<?php echo $id_produk; ?>" tabindex="-1" role="dialog" aria-labelledby="edit-produk<?php echo $id_produk; ?>" aria-hidden="true">
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header" id="loginModalLabel">
-                            <h4 class="modal-title">Edit Produk <?php echo $id_kategori; ?></h4>
+                            <h4 class="modal-title">Edit Produk <?php echo $id_produk; ?></h4>
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
                           </div>
                           <div class="modal-body">
                             <form class="mt-0" method="POST">
                               <div class="form-group">
-                                <input type="hidden" name="id_produk_lama"value="<?php echo $id_kategori; ?>">
-                                <input type="text" name="id_produk_baru" class="form-control mb-2" maxlength = "10" size = "10" value="<?php echo $id_kategori; ?>" placeholder="<?php echo $id_kategori; ?>" required>
+                                <input type="hidden" name="id_produk_lama"value="<?php echo $id_produk; ?>">
+                                <input type="text" name="id_produk_baru" class="form-control mb-2" maxlength = "10" size = "10" value="<?php echo $id_produk; ?>" placeholder="<?php echo $id_produk; ?>" required>
                               </div>
                               <div class="form-group">
-                                <input type="text" name="nama_produk" class="form-control mb-2" maxlength = "50" size = "50" value="<?php echo $id_kategori; ?>" placeholder="<?php echo $id_kategori; ?>" required>
+                                <input type="text" name="nama_produk" class="form-control mb-2" maxlength = "50" size = "50" value="<?php echo $nama_produk; ?>" placeholder="<?php echo $nama_produk; ?>" required>
                               </div>
                               <div class="form-group">
-                                <textarea type="text" name="desc_produk" class="form-control mb-2" maxlength = "100" size = "100" value="<?php echo $id_kategori; ?>" placeholder="<?php echo $id_kategori; ?>" required></textarea>
+                                <textarea type="text" name="desc_produk" class="form-control mb-2" maxlength = "100" size = "100" value="<?php echo $desc_produk; ?>" placeholder="<?php echo $desc_produk; ?>" required></textarea>
                               </div>
                               <div class="form-group">
-                                <input type="text" name="stok" class="form-control mb-2" maxlength = "5" size = "5" value="<?php echo $id_kategori; ?>" placeholder="<?php echo $id_kategori; ?>" required>
+                                <input type="text" name="stok" class="form-control mb-2" maxlength = "5" size = "5" value="<?php echo $stok; ?>" placeholder="<?php echo $stok; ?>" required>
                               </div>
                               <div class="form-group">
-                                <input type="text" name="harga" class="form-control mb-2" maxlength = "10" size = "10"  value="<?php echo $id_kategori; ?>" placeholder="<?php echo $id_kategori; ?>" required>
+                                <input type="text" name="harga" class="form-control mb-2" maxlength = "10" size = "10"  value="<?php echo $harga; ?>" placeholder="<?php echo $harga; ?>" required>
                               </div>
                               <div class="form-group">
                                   <select class="form-control" name="id_kategori">
